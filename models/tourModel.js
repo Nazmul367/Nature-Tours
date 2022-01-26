70308cd2948b9f633f459f5b6ace48291ceac1c6
Nazmul367 <najmulrizvy@gmail.com>
2022-01-26 19:05:14 +0600

    mongoose validation

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,7 +8,9 @@ const tourSchema = new mongoose.Schema({
         type: String,
         required: [true, 'A tour must have a name'],
         unique: true,
-        trim: true
+        trim: true,
+        maxlength: [40, 'A tour name must have less or equal than 40 chars'],
+        minlength: [10, 'A tour name must have less or equal than 10 chars']
     },
     slug: String,
     duration: {
@@ -21,11 +23,17 @@ const tourSchema = new mongoose.Schema({
     },
     difficulty: {
         type: String,
-        required: [true, 'A tour must have a difficulty']
+        required: [true, 'A tour must have a difficulty'],
+        enum: {
+            values: ['easy', 'medium', 'difficult'],
+            message: ['Difficulty is either easy, medium, difficult']
+        }
     },
     ratingsAverage: {
         type: Number,
-        default: 4.5
+        default: 4.5,
+        min: [1, 'Rating must be above 1'],
+        min: [5, 'Rating must be below 5']
     },
     ratingsQuantity: {
         type: Number,
@@ -35,7 +43,15 @@ const tourSchema = new mongoose.Schema({
         type: Number,
         required: [true, 'A tour must have a price']
     },
-    priceDiscount: Number,
+    priceDiscount: {
+        type: Number,
+        validate: {
+            validator: function(val) {
+                return val < this.price
+            },
+            message: 'Discount price ({VALUE}) should be below regular price'
+        }
+    },
     summary: {
         type: String,
         trim: true,
